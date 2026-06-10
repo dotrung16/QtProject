bd631a44d0f1c0801cad0924525cf03392d9d1cb
dotrung16 <dvt1607sla@gmail.com>
2026-06-10 23:54:16 +0700

    Remove build folder from tracking

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_MyApp_FILE /home/do-trung/Qt_Project/build/MyApp)
-set(__QT_DEPLOY_TARGET_MyApp_TYPE EXECUTABLE)
--- build/CMakeFiles/MyApp.dir/MyApp_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MyApp.dir/MyApp_autogen/mocs_compilation.cpp.o: \
- /home/do-trung/Qt_Project/build/MyApp_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
