6ea1e4d18d4833c0cb6d5c2380d3fe1de7e9354f
dotrung16 <dvt1607sla@gmail.com>
2026-06-11 23:41:10 +0700

    Update thing :)

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -2,33 +2,111 @@
 
 #include <QWidget>
 #include <QPushButton>
-#include <QLabel>
 #include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QListWidget>
+#include <QFileDialog>
+#include <QStyle>
+#include <QSize>
+
+#include <QMediaPlayer>
+#include <QAudioOutput>
+#include <QUrl>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
-	setWindowTitle("My App");
-	resize(800, 600);
+	setWindowTitle("Music Player");
+	resize(700, 500);
 
 	QWidget *central = new QWidget(this);
 	setCentralWidget(central);
 
-	QVBoxLayout *layout = new QVBoxLayout(central);
-	layout->setAlignment(Qt::AlignCenter);
-	layout->setSpacing(20);
+	QVBoxLayout *mainLayout = new QVBoxLayout(central);
+	mainLayout->setContentsMargins(0, 0, 0, 0);
+	mainLayout->setSpacing(0);
 
-	m_label = new QLabel("Press the button!", central);
-	m_label->setAlignment(Qt::AlignCenter);
+	m_playlistWidget = new QListWidget();
+	m_playlistWidget->setStyleSheet(
+		"QListWidget { border: none; padding: 10px; font-size: 14px; }"
+		"QListWidget::item { padding: 5px; border-radius: 5px; }"
+		"QListWidget::item::selected { background-color: #E95420; color: white }"
+	);
+	mainLayout->addWidget(m_playlistWidget);
 
-	m_button = new QPushButton("Hello!", central);
-	m_button->setFixedSize(200, 50);
+	QWidget *controlDock = new QWidget();
+	controlDock->setStyleSheet(
+		"QWidget { background-color: #dcdcdc; border-top: 1px solid #b3b3b3 }"
+		"QPushButton { background: transparent; border: none; padding 10px; border-radius: 15px }"
+		"QPushButton::hover { background-color: #c8c8c8 }"
+		"QPushButton::pressed { background-color: #E95420 }"
+	);
 
-	layout->addWidget(m_label);
-	layout->addWidget(m_button);
+	QHBoxLayout *controlLayout = new QHBoxLayout(controlDock);
+	controlLayout->setContentsMargins(20, 10, 20, 10);
+	controlLayout->setAlignment(Qt::AlignCenter);
 
-	connect(m_button, &QPushButton::clicked, this, &MainWindow::onButtonClicked);
+	QSize stdSize(32, 32);
+	QSize playSize(48, 48);
+
+	m_btnLoad = new QPushButton();
+	m_btnLoad->setIcon(style()->standardIcon(QStyle::SP_DialogOpenButton));
+	m_btnLoad->setIconSize(stdSize);
+	m_btnLoad->setToolTip("Load Music");
+
+	m_btnPlay = new QPushButton();
+	m_btnPlay->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+	m_btnPlay->setIconSize(playSize);
+
+	m_btnPause = new QPushButton();
+	m_btnPause->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
+	m_btnPause->setIconSize(stdSize);
+
+	m_btnStop = new QPushButton();
+	m_btnStop->setIcon(style()->standardIcon(QStyle::SP_MediaStop));
+	m_btnStop->setIconSize(stdSize);
+
+	controlLayout->addWidget(m_btnLoad);
+	controlLayout->addSpacing(20);
+	controlLayout->addWidget(m_btnPlay);
+	controlLayout->addWidget(m_btnPause);
+	controlLayout->addWidget(m_btnStop);
+
+	mainLayout->addWidget(controlDock);
+
+	m_player = new QMediaPlayer(this);
+	m_audioOutput = new QAudioOutput(this);
+	m_player->setAudioOutput(m_audioOutput);
+	m_audioOutput->setVolume(0.5);
+
+	connect(m_btnLoad, &QPushButton::clicked, this, &MainWindow::onLoadClicked);
+	connect(m_btnPlay, &QPushButton::clicked, this, &MainWindow::onPlayClicked);
+	connect(m_btnPause, &QPushButton::clicked, this, &MainWindow::onPauseClicked);
+	connect(m_btnStop, &QPushButton::clicked, this, &MainWindow::onStopClicked);
 }
 
-void MainWindow::onButtonClicked() {
-	m_label->setText("Button was clicked!");
+void MainWindow::onLoadClicked() {
+	QStringList files = QFileDialog::getOpenFileNames(this, "Select Music Files", "", "Audio Files (*.mp3 *wav *.flac)");
+
+	for (const QString &file : files) {
+		m_playlistWidget->addItem(file);
+	}
 }
+
+void MainWindow::onPlayClicked() {
+	if (m_playlistWidget->currentItem()) {
+		QString filePath = m_playlistWidget->currentItem()->text();
+		m_player->setSource(QUrl::fromLocalFile(filePath));
+		m_player->play();
+	}
+}
+
+void MainWindow::onPauseClicked() {
+	m_player->pause();
+}
+
+void MainWindow::onStopClicked() {
+	m_player->stop();
+}
+
+
+
 
--- MainWindow.h
+++ MainWindow.h
@@ -4,8 +4,11 @@
 
 QT_BEGIN_NAMESPACE
 class QPushButton;
-class QLabel;
 class QVBoxLayout;
+class QHBoxLayout;
+class QListWidget;
+class QMediaPlayer;
+class QAudioOutput;
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow {
@@ -15,9 +18,18 @@ public:
 	explicit MainWindow(QWidget *parent = nullptr);
 	~MainWindow() override = default;
 private slots:
-	void onButtonClicked();
+	void onLoadClicked();
+	void onPlayClicked();
+	void onPauseClicked();
+	void onStopClicked();
 private:
-    QPushButton *m_button;
-    QLabel *m_label;
+    QListWidget *m_playlistWidget = nullptr;
+    QPushButton *m_btnLoad = nullptr;
+    QPushButton *m_btnPlay = nullptr;
+    QPushButton *m_btnPause = nullptr;
+    QPushButton *m_btnStop = nullptr;
+
+    QMediaPlayer *m_player = nullptr;
+    QAudioOutput *m_audioOutput = nullptr;
 };
 
