67a3a7e99952d131969729e8b41559a3c62658c3
dotrung16 <dvt1607sla@gmail.com>
2026-06-14 00:15:20 +0700

    Update things :)

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -8,6 +8,13 @@
 #include <QFileDialog>
 #include <QStyle>
 #include <QSize>
+#include <QFileInfo>
+#include <QSlider>
+#include <QLabel>
+#include <QTime>
+#include <QDragEnterEvent>
+#include <QDropEvent>
+#include <QMimeData>
 
 #include <QMediaPlayer>
 #include <QAudioOutput>
@@ -16,6 +23,7 @@
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
 	setWindowTitle("Music Player");
 	resize(700, 500);
+	setAcceptDrops(true);
 
 	QWidget *central = new QWidget(this);
 	setCentralWidget(central);
@@ -38,38 +46,68 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
 		"QPushButton { background: transparent; border: none; padding: 10px; border-radius: 15px }"
 		"QPushButton::hover { background-color: #c8c8c8 }"
 		"QPushButton::pressed { background-color: #E95420 }"
+		"QPushButton::disabled { opacity: 0.5; }"
 	);
 
-	QHBoxLayout *controlLayout = new QHBoxLayout(controlDock);
+	QVBoxLayout *dockLayout = new QVBoxLayout(controlDock);
+
+	m_nowPlayingLabel = new QLabel("Ready", controlDock);
+	m_nowPlayingLabel->setAlignment(Qt::AlignCenter);
+	m_nowPlayingLabel->setStyleSheet("font-weight: bold; color: #333; margin-bottom: 5px;");
+	dockLayout->addWidget(m_nowPlayingLabel);
+
+	QHBoxLayout *progressLayout = new QHBoxLayout();
+	m_timeLabel = new QLabel("00:00 / 00:00", controlDock);
+	m_progressSlider = new QSlider(Qt::Horizontal, controlDock);
+	m_progressSlider->setRange(0, 0);
+
+	progressLayout->addWidget(m_progressSlider);
+	progressLayout->addWidget(m_timeLabel);
+	dockLayout->addLayout(progressLayout);
+
+	QHBoxLayout *controlLayout = new QHBoxLayout();
 	controlLayout->setContentsMargins(20, 10, 20, 10);
 	controlLayout->setAlignment(Qt::AlignCenter);
 
-	QSize stdSize(32, 32);
-	QSize playSize(48, 48);
-
 	m_btnLoad = new QPushButton(controlDock);
 	m_btnLoad->setIcon(style()->standardIcon(QStyle::SP_DialogOpenButton));
-	m_btnLoad->setIconSize(stdSize);
+	m_btnLoad->setIconSize(QSize(32, 32));
 	m_btnLoad->setToolTip("Load Music");
 
 	m_btnPlay = new QPushButton(controlDock);
 	m_btnPlay->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
-	m_btnPlay->setIconSize(playSize);
+	m_btnPlay->setIconSize(QSize(48, 48));
+	m_btnPlay->setToolTip("Play/Restart");
 
 	m_btnPause = new QPushButton(controlDock);
 	m_btnPause->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
-	m_btnPause->setIconSize(stdSize);
+	m_btnPause->setIconSize(QSize(32, 32));
+	m_btnPause->setToolTip("Pause");
 
 	m_btnStop = new QPushButton(controlDock);
 	m_btnStop->setIcon(style()->standardIcon(QStyle::SP_MediaStop));
-	m_btnStop->setIconSize(stdSize);
+	m_btnStop->setIconSize(QSize(32, 32));
+	m_btnStop->setToolTip("Stop");
+
+	QHBoxLayout *volumeLayout = new QHBoxLayout();
+	QLabel *volumeIcon = new QLabel(controlDock);
+	volumeIcon->setPixmap(style()->standardIcon(QStyle::SP_MediaVolume).pixmap(24, 24));
+	m_volumeSlider = new QSlider(Qt::Horizontal, controlDock);
+	m_volumeSlider->setRange(0, 100);
+	m_volumeSlider->setValue(50);
+	m_volumeSlider->setMaximumWidth(100);
+	volumeLayout->addWidget(volumeIcon);
+	volumeLayout->addWidget(m_volumeSlider);
 
 	controlLayout->addWidget(m_btnLoad);
 	controlLayout->addSpacing(20);
 	controlLayout->addWidget(m_btnPlay);
 	controlLayout->addWidget(m_btnPause);
 	controlLayout->addWidget(m_btnStop);
+	controlLayout->addSpacing(20);
+	controlLayout->addLayout(volumeLayout);
 
+	dockLayout->addLayout(controlLayout);
 	mainLayout->addWidget(controlDock);
 
 	m_player = new QMediaPlayer(this);
@@ -81,23 +119,51 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
 	connect(m_btnPlay, &QPushButton::clicked, this, &MainWindow::onPlayClicked);
 	connect(m_btnPause, &QPushButton::clicked, this, &MainWindow::onPauseClicked);
 	connect(m_btnStop, &QPushButton::clicked, this, &MainWindow::onStopClicked);
+
+	connect(m_playlistWidget, &QListWidget::itemDoubleClicked, this, &MainWindow::onPlayClicked);
+
+	connect(m_player, &QMediaPlayer::durationChanged, this, &MainWindow::onDurationChanged);
+	connect(m_player, &QMediaPlayer::positionChanged, this, &MainWindow::onPositionChanged);
+	connect(m_progressSlider, &QSlider::sliderMoved, this, &MainWindow::onSeekRequested);
+	connect(m_volumeSlider, &QSlider::valueChanged, this, &MainWindow::onVolumeChanged);
+	connect(m_player, &QMediaPlayer::mediaStatusChanged, this, &MainWindow::onMediaStatusChanged);
+	connect(m_player, &QMediaPlayer::playbackStateChanged, this, &MainWindow::onPlaybackStateChanged);
+
+	connect(m_player, &QMediaPlayer::errorOccurred, this, [this](QMediaPlayer::Error error, const QString &errorString) {
+		qWarning() << "Media Player Error: " << errorString;
+	});
+
+	updateButtonStates();
 }
 
 void MainWindow::onLoadClicked() {
 	QStringList files = QFileDialog::getOpenFileNames(this, "Select Music Files", "", "Audio Files (*.mp3 *.wav *.flac)");
 
 	for (const QString &file : files) {
-		m_playlistWidget->addItem(file);
+		QFileInfo fileInfo(file);
+		QListWidgetItem *item = new QListWidgetItem(fileInfo.fileName());
+		item->setData(Qt::UserRole, file);
+		m_playlistWidget->addItem(item);
 	}
+
+	if (m_playlistWidget->count() > 0 && !m_playlistWidget->currentItem()) {
+		m_playlistWidget->setCurrentRow(0);
+	}
+
+	updateButtonStates();
 }
 
 void MainWindow::onPlayClicked() {
 	if (m_playlistWidget->currentItem()) {
-		QString fileUrl = m_playlistWidget->currentItem()->text();
+		QString filePath = m_playlistWidget->currentItem()->data(Qt::UserRole).toString();
+		QUrl fileUrl = QUrl::fromLocalFile(filePath);
 		if (m_player->source() != fileUrl) {
 			m_player->setSource(fileUrl);
 		}
 		m_player->play();
+
+		m_nowPlayingLabel->setText("Playing: " + m_playlistWidget->currentItem()->text());
+		m_playlistWidget->scrollToItem(m_playlistWidget->currentItem());
 	}
 }
 
@@ -107,8 +173,85 @@ void MainWindow::onPauseClicked() {
 
 void MainWindow::onStopClicked() {
 	m_player->stop();
+	m_progressSlider->setValue(0);
+	m_timeLabel->setText(formatTime(0) + " / " + formatTime(m_player->duration()));
+	m_nowPlayingLabel->setText("Ready");
 }
 
+void MainWindow::onDurationChanged(qint64 duration) {
+	m_progressSlider->setMaximum(static_cast<int>(duration));
+	m_timeLabel->setText(formatTime(0) + " / " + formatTime(duration));
+}
 
+void MainWindow::onPositionChanged(qint64 position) {
+	if (!m_progressSlider->isSliderDown()) {
+		m_progressSlider->setValue(static_cast<int>(position));
+	}
+	m_timeLabel->setText(formatTime(position) + " / " + formatTime(m_player->duration()));
+}
 
+void MainWindow::onSeekRequested(int position) {
+	m_player->setPosition(static_cast<qint64>(position));
+}
 
+void MainWindow::onVolumeChanged(int volume) {
+	m_audioOutput->setVolume(volume / 100.0f);
+}
+
+void MainWindow::onMediaStatusChanged(QMediaPlayer::MediaStatus status) {
+	if (status == QMediaPlayer::EndOfMedia) {
+		int currentRow = m_playlistWidget->currentRow();
+		if (currentRow < m_playlistWidget->count() - 1) {
+			m_playlistWidget->setCurrentRow(currentRow + 1);
+			onPlayClicked();
+		} 
+		else {
+			m_player->stop();
+		}
+	}
+}
+
+void MainWindow::onPlaybackStateChanged(QMediaPlayer::PlaybackState state) {
+	updateButtonStates();
+}
+
+void MainWindow::updateButtonStates() {
+	bool hasItems = m_playlistWidget->count() > 0;
+	bool isPlaying = m_player->playbackState() == QMediaPlayer::PlayingState;
+	bool isPaused = m_player->playbackState() == QMediaPlayer::PausedState;
+
+	m_btnPlay->setEnabled(hasItems);
+	m_btnPause->setEnabled(isPlaying);
+	m_btnStop->setEnabled(isPlaying || isPaused);
+}
+
+QString MainWindow::formatTime(qint64 milliseconds) {
+	return QTime(0, 0).addMSecs(milliseconds).toString("mm:ss"); 
+}
+
+void MainWindow::dragEnterEvent(QDragEnterEvent *event) {
+	if (event->mimeData()->hasUrls()) {
+		event->acceptProposedAction();
+	}
+}
+
+void MainWindow::dropEvent(QDropEvent *event) {
+	for (const QUrl &url : event->mimeData()->urls()) {
+		if (url.isLocalFile()) {
+			QString filePath = url.toLocalFile();
+
+			if (filePath.endsWith(".mp3", Qt::CaseInsensitive) || filePath.endsWith(".wav", Qt::CaseInsensitive) || filePath.endsWith(".flac", Qt::CaseInsensitive)) {
+				QFileInfo fileInfo(filePath);
+				QListWidgetItem *item = new QListWidgetItem(fileInfo.fileName());
+				item->setData(Qt::UserRole, filePath);
+				m_playlistWidget->addItem(item);
+			}
+		}
+	}
+
+	if (m_playlistWidget->count() > 0 && !m_playlistWidget->currentItem()) {
+		m_playlistWidget->setCurrentRow(0);
+	}
+
+	updateButtonStates(); 
+}
--- MainWindow.h
+++ MainWindow.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QMediaPlayer>
 
 QT_BEGIN_NAMESPACE
 class QPushButton;
@@ -9,6 +10,8 @@ class QHBoxLayout;
 class QListWidget;
 class QMediaPlayer;
 class QAudioOutput;
+class QSlider;
+class QLabel;
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow {
@@ -22,14 +25,32 @@ private slots:
 	void onPlayClicked();
 	void onPauseClicked();
 	void onStopClicked();
+
+	void onDurationChanged(qint64 duration);
+	void onPositionChanged(qint64 position);
+	void onSeekRequested(int position);
+	void onVolumeChanged(int volume);
+	void onMediaStatusChanged(QMediaPlayer::MediaStatus status);
+	void onPlaybackStateChanged(QMediaPlayer::PlaybackState state);
 private:
+	void updateButtonStates();
+	QString formatTime(qint64 milliseconds);
+
     QListWidget *m_playlistWidget = nullptr;
     QPushButton *m_btnLoad = nullptr;
     QPushButton *m_btnPlay = nullptr;
     QPushButton *m_btnPause = nullptr;
     QPushButton *m_btnStop = nullptr;
 
+    QSlider *m_progressSlider = nullptr;
+    QLabel *m_timeLabel = nullptr;
+    QSlider *m_volumeSlider = nullptr;
+    QLabel *m_nowPlayingLabel = nullptr;
+
     QMediaPlayer *m_player = nullptr;
     QAudioOutput *m_audioOutput = nullptr;
+protected:
+	void dragEnterEvent(QDragEnterEvent *event) override;
+	void dropEvent(QDropEvent *event) override;
 };
 
